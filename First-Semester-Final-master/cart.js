b34d3e19579ea34a7a5bb19674dae206a4269281
9Kevinzhao <9kzhao@stpatrick.org>
2016-12-12 13:57:14 -0600

    Add files via upload

---
+++ First-Semester-Final-master/cart.js
@@ -0,0 +1,37 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor(itemList,quantity){
+ this.itemList =[];
+ this.quantity = [];
+}
+
+
+  //Type the instance functions below this comment.
+function cartTotal(){
+  let total=0
+  for(var i=0;i<quantity.length;i++){
+    total += quantity[i]
+  }
+  return total
+}
+
+function addItem(number){
+  this.itemList.push(Item)
+  this.quantity.push(number)
+}
+
+function subCart(day){
+  let subArray =[]
+  if (Item.shippingTime == day){
+    subArray.push(Item.name);
+  }
+  return subArray;
+}
+
+function delivery()
+let earliest = itemList[0].shippingTime
+let lastest = itemList[0].shippingTime
+for (var i=0, i<itemList.length; i++){
+  if
+}
+}
---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor(n,p,s){
+  this.name = n
+  this.price = p
+  this.shippingTime = s
+  };
+
+
+}
+
+//Create your three test items below this comment.
+let soap = new Item("soap", 5, 3)
